2bfbad99a27057635fec9896277d8758f0842de0
mikepicard <mikepicard01@gmail.com>
2016-02-19 00:36:32 -0500

    Update sniff.sh

--- sniff.sh
+++ sniff.sh
@@ -3,9 +3,10 @@
 #comment: poor mans sniffer
 
 proto=""
+int=""
 
 usage() {
-    echo "usage: $0 -s <proto>"
+    echo "usage: $0 -p <proto>"
     echo "protocol options:"
     echo "  http"
     echo "  ftp - coming soon"
@@ -17,10 +18,8 @@ usage() {
 
 while [ -n "$1" ] ; do 
     case "$1" in
-        -s)
+        -p)
             shift; proto="$1";;
-        -u)
-            shift; int="$1";;
         *)
             echo "invalid switch"
             usage;;
@@ -32,28 +31,28 @@ if [[ $proto == *"http"* ]] ; then
     stdbuf -oL -eL tcpdump -A -s 10240 "tcp port 80 and (((ip[2:2] - ((ip[0]&0xf)<<2)) - ((tcp[12]&0xf0)>>2)) != 0)" | egrep -a --line-buffered ".+(GET |HTTP\/|POST )|^[A-Za-z0-9-]+: " | perl -nle 'BEGIN{$|=1} { s/.*?(GET |HTTP\/[0-9.]* |POST )/\n$1/g; print }' | while read line ; do 
         #All Methods are black on blue 
         if [[ $line == *"OPTIONS"* ]] ; then 
-            echo -e '\E[30;44m'"$line\033[0m" 
+            echo -e '\E[0;34m'"$line\033[0m" 
         elif [[ $line == *"GET"* ]] ; then 
-            echo -e '\E[30;44m'"$line\033[0m"
+            echo -e '\E[0;34m'"$line\033[0m"
         elif [[ $line == *"HEAD"* ]] ; then
-            echo -e '\E[30;44m'"$line\033[0m"
+            echo -e '\E[0;34m'"$line\033[0m"
         elif [[ $line == *"PUT"* ]] ; then
-            echo -e '\E[30;44m'"$line\033[0m"
+            echo -e '\E[0;34m'"$line\033[0m"
         elif [[ $line == *"DELETE"* ]] ; then
-            echo -e '\E[30;44m'"$line\033[0m"
+            echo -e '\E[0;34m'"$line\033[0m"
         elif [[ $line == *"TRACE"* ]] ; then
-            echo -e '\E[30;44m'"$line\033[0m"
+            echo -e '\E[0;34m'"$line\033[0m"
         elif [[ $line == *"CONNECT"* ]] ; then
-            echo -e '\E[30;44m'"$line\033[0m"
+            echo -e '\E[0;34m'"$line\033[0m"
         #Status codes vary
         elif [[ $line == *"HTTP/1.1 2"* ]] ; then
-            echo -e '\E[30;42m'"$line\033[0m" #20X Success black on green
+            echo -e '\E[1;32m'"$line\033[0m" #20X Success black on green
         elif [[ $line == *"HTTP/1.1 3"* ]] ; then
-            echo -e '\E[30;45m'"$line\033[0m" #3XX Redirection black on magenta
+            echo -e '\E[1;35m'"$line\033[0m" #3XX Redirection black on magenta
         elif [[ $line == *"HTTP/1.1 4"* ]] ; then
-            echo -e '\E[30;43m'"$line\033[0m" #4XX Client error black on yellow 
+            echo -e '\E[1;33m'"$line\033[0m" #4XX Client error black on yellow 
         elif [[ $line == *"HTTP/1.1 5"* ]] ; then
-            echo -e '\E[37;41m'"$line\033[0m" #5XX Server error white on red
+            echo -e '\E[1;41m'"$line\033[0m" #5XX Server error white on red
         #Common headers are cyan
         elif [[ $line == *":"* ]] ; then
             echo -e '\E[1;36m'"$line\033[0m"
